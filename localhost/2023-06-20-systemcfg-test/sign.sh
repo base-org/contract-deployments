fce42189298c571de29332915edcbcaf62692d61
Anika Raghuvanshi <anika.raghuvanshi@coinbase.com>
2023-06-22 16:39:30 +0100

    Final execution occurs by anyone

--- localhost/2023-06-20-systemcfg-test/sign.sh
+++ localhost/2023-06-20-systemcfg-test/sign.sh
@@ -4,6 +4,6 @@ set -e
 source ../.env
 source .env
 
-payload=$(forge script --via-ir --rpc-url ${L1_RPC_URL} TransferSystemConfigOwner --sender ${LOCALHOST_ADDR_1} --private-key ${PRIVATE_KEY} --sig "sign()" | grep -A1 vvvvvvvv | grep -v vvvvvvvv)
+payload=$(forge script --via-ir --rpc-url ${L1_RPC_URL} TransferSystemConfigOwner --sig "sign()" | grep -A1 vvvvvvvv | grep -v vvvvvvvv)
 cd lib/base-contracts
 echo "${payload}" | go run ./cmd/sign --private-key ${PRIVATE_KEY}
