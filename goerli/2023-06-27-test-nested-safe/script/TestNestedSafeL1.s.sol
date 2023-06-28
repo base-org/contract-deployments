f5bc097c391f9d11b599da71a77f636b3e20c714
Francis Li <huiyi.li@outlook.com>
2023-06-28 15:38:24 -0700

    [Base] Setup nested gnosis safe test scripts (#50)

---
+++ goerli/2023-06-27-test-nested-safe/script/TestNestedSafeL1.s.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.15;
+
+import "@base-contracts/src/TestOwner.sol";
+import "@base-contracts/script/universal/NestedMultisigBuilder.sol";
+
+contract TestNestedSafeL1 is NestedMultisigBuilder {
+    address constant internal TEST_CONTRACT = 0x49f891c8032f182CCcb6bb3854B75B3F6d3E26A0;
+    address constant internal L1_NESTED_SAFE = 0x444b8C3E4eA49cE15A93D96AfA83D421F6049524;
+
+    function _postCheck() internal override view {
+        // perform post execution checks
+    }
+
+    function _buildCalls() internal override view returns (IMulticall3.Call3[] memory) {
+        IMulticall3.Call3[] memory calls = new IMulticall3.Call3[](1);
+
+        calls[0] = IMulticall3.Call3({
+            target: TEST_CONTRACT,
+            allowFailure: false,
+            callData: abi.encodeCall(
+                TestOwner.increment,
+                ()
+            )
+        });
+
+        return calls;
+    }
+
+    function _ownerSafe() internal override view returns (address) {
+        return L1_NESTED_SAFE;
+    }
+}
