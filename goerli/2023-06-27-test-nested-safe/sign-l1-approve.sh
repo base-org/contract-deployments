f50fcd87153c051d31256d7120664b9325e58adc
Francis Li <francis.li@coinbase.com>
2023-06-28 14:28:45 -0700

    [Base] Test nested gnosis safe

---
+++ goerli/2023-06-27-test-nested-safe/sign-l1-approve.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -eux
+
+source ../.env
+source .env
+source .env.local
+
+payload=$(forge script --via-ir --rpc-url ${L1_RPC_URL} TestNestedSafeL1 --sig "signApproval(address)" ${SIGNER_SAFE_1_L1} | grep -A1 vvvvvvvv | grep -v vvvvvvvv)
+cd lib/base-contracts
+echo "${payload}" | go run ./cmd/sign --ledger --hd-path "m/44'/60'/${LEDGER_ACCOUNT}'/0/0"
