4bb6c5e426d71e6c1a3fe7a340b6871928768038
Michael de Hoog <michael.dehoog@coinbase.com>
2023-07-17 20:57:29 -1000

    Print forge output to stderr

--- goerli-devnet/2023-06-22-test-nested-safe/sign-l1.sh
+++ goerli-devnet/2023-06-22-test-nested-safe/sign-l1.sh
@@ -5,7 +5,7 @@ source ../.env
 source .env
 source .env.local
 
-payload=$(forge script --via-ir --rpc-url ${L1_RPC_URL} TestNestedSafeL1 --sig "signTransaction(address)" ${SIGNER_SAFE_2_L1} | grep -A1 vvvvvvvv | grep -v vvvvvvvv)
+payload=$(forge script --via-ir --rpc-url ${L1_RPC_URL} TestNestedSafeL1 --sig "signTransaction(address)" ${SIGNER_SAFE_2_L1} | tee /dev/stderr | grep -A1 vvvvvvvv | grep -v vvvvvvvv)
 cd lib/base-contracts
 echo "${payload}" | go run ./cmd/sign --private-key ${PRIVATE_KEY}
 
