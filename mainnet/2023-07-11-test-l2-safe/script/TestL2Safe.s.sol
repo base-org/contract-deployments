046b3f7247c26eafd9b15a63b0baa2e8db8140c5
Anika Raghuvanshi <anika.raghuvanshi@coinbase.com>
2023-07-20 01:02:51 +0200

    Contracts now deployed

--- mainnet/2023-07-11-test-l2-safe/script/TestL2Safe.s.sol
+++ mainnet/2023-07-11-test-l2-safe/script/TestL2Safe.s.sol
@@ -8,12 +8,11 @@ import "@base-contracts/script/universal/MultisigBuilder.sol";
 import "@eth-optimism-bedrock/contracts/universal/ProxyAdmin.sol";
 
 contract TestL2Safe is MultisigBuilder {
-    address constant internal PROXY_ADMIN_CONTRACT = 0x5170619f82f277c2B6BD3A9173CCb74D10B4B7d1;
-    address constant internal PROXY_CONTRACT = 0x96ffD33D44792B73C573B9D773A2Af4623924aEE;
-    address constant internal OLD_IMPLEMENTATION = 0xa56bCd3A713e2857b453ED38aD3052Bb7953e798;
+    address constant internal PROXY_ADMIN_CONTRACT = 0x21EB1F4C0b15336b83Ec574842C1fcfC9DF6d698;
+    address constant internal PROXY_CONTRACT = 0xCFf6EbA9DD666C6AE51Ec35a3999A9715e5aAa87;
+    address constant internal OLD_IMPLEMENTATION = 0x0a9F0F5B38951955ff68263510AD45Af71468D55;
     address constant internal NEW_IMPLEMENTATION = 0x004BC95c786dc50b42cC573458cC39ba82d98C09;
-    address constant internal L2_SAFE = 0xd94E416cf2c7167608B2515B7e4102B41efff94f;
-    address constant internal NEW_OWNER = address(1);
+    address constant internal L2_SAFE = 0x25afe4249e4410DB5F20c6421d96E0F02C0f4E3B;
 
     function _postCheck() internal override view {
         ProxyAdmin proxyAdmin = ProxyAdmin(PROXY_ADMIN_CONTRACT);
@@ -23,9 +22,6 @@ contract TestL2Safe is MultisigBuilder {
             proxyAdmin.getProxyImplementation(PROXY_CONTRACT) == NEW_IMPLEMENTATION,
             "TestL2Safe: implementation did not get set"
         );
-
-        // Check proxy admin owner was upgraded
-        require(proxyAdmin.owner() == NEW_OWNER, "TestL2Safe: proxyAdmin owner is incorrect");
     }
 
     function _buildCalls() internal override view returns (IMulticall3.Call3[] memory) {
@@ -35,25 +31,15 @@ contract TestL2Safe is MultisigBuilder {
             "TestL2Safe: implementation did not get set"
         );
 
-        IMulticall3.Call3[] memory calls = new IMulticall3.Call3[](2);
+        IMulticall3.Call3[] memory calls = new IMulticall3.Call3[](1);
 
-        // Test 1: Upgrade a contract implementation
+        // Upgrade a contract implementation
         calls[0] = IMulticall3.Call3({
             target: PROXY_ADMIN_CONTRACT,
             allowFailure: false,
             callData: abi.encodeCall(
                 ProxyAdmin.upgrade,
                 (payable(PROXY_CONTRACT), NEW_IMPLEMENTATION)
-            )
-        });
-
-        // Test 2: Transfer ownership of proxy admin
-        calls[1] = IMulticall3.Call3({
-            target: PROXY_ADMIN_CONTRACT,
-            allowFailure: false,
-            callData: abi.encodeCall(
-                OwnableUpgradeable.transferOwnership,
-                (NEW_OWNER)
             )
         });
 
