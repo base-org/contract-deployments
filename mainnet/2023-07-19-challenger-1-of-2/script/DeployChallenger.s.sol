1fab288157f3093fa0d79dfc8e7a8cd91adbbda6
Anika Raghuvanshi <anika.raghuvanshi@coinbase.com>
2023-07-20 01:11:00 +0200

    Deploy record

--- mainnet/2023-07-19-challenger-1-of-2/script/DeployChallenger.s.sol
+++ mainnet/2023-07-19-challenger-1-of-2/script/DeployChallenger.s.sol
@@ -9,24 +9,17 @@ contract DeployChallenger is Script {
     function run(address deployer, address signer1, address signer2, address l2OutputOracleProxy) public {
         vm.broadcast(deployer);
         Challenger1of2 challenger = new Challenger1of2(signer1, signer2, l2OutputOracleProxy);
-        require(challenger.OP_SIGNER() == signer1, "OP_SIGNER not set correctly");
-        require(challenger.OTHER_SIGNER() == signer2, "OTHER_SIGNER not set correctly");
-        require(challenger.L2_OUTPUT_ORACLE_PROXY() == l2OutputOracleProxy, "L2_OUTPUT_ORACLE_PROXY not set correctly");
-        console.log(address(challenger));
+        require(challenger.OP_SIGNER() == signer1, "DeployChallenger: OP_SIGNER not set correctly");
+        require(challenger.OTHER_SIGNER() == signer2, "DeployChallenger: OTHER_SIGNER not set correctly");
+        require(challenger.L2_OUTPUT_ORACLE_PROXY() == l2OutputOracleProxy, "DeployChallenger: L2_OUTPUT_ORACLE_PROXY not set correctly");
 
-        require(l2OutputOracleProxy != address(0), "Deploy: l2OutputOracleProxy is zero address");
+        require(l2OutputOracleProxy != address(0), "DeployChallenger: l2OutputOracleProxy is zero address");
 
         L2OutputOracle existingL2OO = L2OutputOracle(l2OutputOracleProxy);
         uint256 oldSubmissionInterval = existingL2OO.SUBMISSION_INTERVAL();
         uint256 oldL2BlockTime = existingL2OO.L2_BLOCK_TIME();
         address oldProposer = existingL2OO.PROPOSER();
         uint256 oldFinalizationPeriodSeconds = existingL2OO.FINALIZATION_PERIOD_SECONDS();
-
-        console.log(oldSubmissionInterval);
-        console.log(oldL2BlockTime);
-        console.log(oldProposer);
-        console.log(oldFinalizationPeriodSeconds);
-        console.log(existingL2OO.CHALLENGER());
 
         // Deploy L2OutputOracle new implementation with new challenger
         vm.broadcast(deployer);
@@ -40,13 +33,13 @@ contract DeployChallenger is Script {
             _finalizationPeriodSeconds: oldFinalizationPeriodSeconds            
         });
 
-        require(l2OutputOracleImpl.CHALLENGER() == address(challenger), "Deploy: l2OutputOracle challenger is incorrect");
+        require(l2OutputOracleImpl.CHALLENGER() == address(challenger), "DeployChallenger: l2OutputOracle challenger is incorrect");
 
-        require(l2OutputOracleImpl.SUBMISSION_INTERVAL() == oldSubmissionInterval, "Deploy: l2OutputOracle submissionInterval is incorrect");
-        require(l2OutputOracleImpl.L2_BLOCK_TIME() == oldL2BlockTime, "Deploy: l2OutputOracle l2BlockTime is incorrect");
-        require(l2OutputOracleImpl.startingBlockNumber() == 0, "Deploy: l2OutputOracle startingBlockNumber is incorrect");
-        require(l2OutputOracleImpl.startingTimestamp() == 0, "Deploy: l2OutputOracle startingTimestamp is incorrect");
-        require(l2OutputOracleImpl.PROPOSER() == oldProposer, "Deploy: l2OutputOracle proposer is incorrect");
-        require(l2OutputOracleImpl.FINALIZATION_PERIOD_SECONDS() == oldFinalizationPeriodSeconds, "Deploy: l2OutputOracle finalizationPeriodSeconds is incorrect");
+        require(l2OutputOracleImpl.SUBMISSION_INTERVAL() == oldSubmissionInterval, "DeployChallenger: l2OutputOracle submissionInterval is incorrect");
+        require(l2OutputOracleImpl.L2_BLOCK_TIME() == oldL2BlockTime, "DeployChallenger: l2OutputOracle l2BlockTime is incorrect");
+        require(l2OutputOracleImpl.startingBlockNumber() == 0, "DeployChallenger: l2OutputOracle startingBlockNumber is incorrect");
+        require(l2OutputOracleImpl.startingTimestamp() == 0, "DeployChallenger: l2OutputOracle startingTimestamp is incorrect");
+        require(l2OutputOracleImpl.PROPOSER() == oldProposer, "DeployChallenger: l2OutputOracle proposer is incorrect");
+        require(l2OutputOracleImpl.FINALIZATION_PERIOD_SECONDS() == oldFinalizationPeriodSeconds, "DeployChallenger: l2OutputOracle finalizationPeriodSeconds is incorrect");
     }
 }
