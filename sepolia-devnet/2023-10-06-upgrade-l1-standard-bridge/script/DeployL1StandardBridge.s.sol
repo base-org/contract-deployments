536e2db331c1c6d0d13847b1e1a83277240e52eb
Michael de Hoog <michael.dehoog@coinbase.com>
2023-10-06 19:54:27 -1000

    Upgrade L1StandardBridge on sepolia devnet

---
+++ sepolia-devnet/2023-10-06-upgrade-l1-standard-bridge/script/DeployL1StandardBridge.s.sol
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.15;
+
+import "src/L1/L1StandardBridge.sol";
+import "src/universal/CrossDomainMessenger.sol";
+import "forge-std/Script.sol";
+
+contract DeployL1StandardBridgeImplementation is Script {
+    address constant internal DEPLOYER = 0x24A1704636AB7083eaC56294aFF13e1651997638;
+    address constant internal L1_STANDARD_BRIDGE_PROXY = 0x5638e55db5Fcf7A58df525F1098E8569C8DbA80c;
+
+    function implSalt() public returns (bytes32) {
+        return keccak256(bytes(vm.envOr("IMPL_SALT", string("base"))));
+    }
+
+    function run() public {
+        L1StandardBridge proxy = L1StandardBridge(payable(L1_STANDARD_BRIDGE_PROXY));
+
+        CrossDomainMessenger messenger = proxy.messenger();
+        console.log("Current L1CrossDomainMessenger value: %s", address(messenger));
+
+        string memory version = proxy.version();
+        console.log("Current L1StandardBridge version: %s", version);
+
+        vm.broadcast(DEPLOYER);
+        L1StandardBridge bridge = new L1StandardBridge{ salt: implSalt() }();
+        console.log("New L1StandardBridge deployed at %s", address(bridge));
+
+        version = bridge.version();
+        console.log("New L1StandardBridge version: %s", version);
+
+        require(address(bridge.MESSENGER()) == address(0));
+        require(address(bridge.messenger()) == address(0));
+        require(address(bridge.OTHER_BRIDGE()) == Predeploys.L2_STANDARD_BRIDGE);
+        require(address(bridge.otherBridge()) == Predeploys.L2_STANDARD_BRIDGE);
+    }
+}
