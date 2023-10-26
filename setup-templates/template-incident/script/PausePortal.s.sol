4694385f4f2ba97b8add108f8916c9a45c7309e6
anikaraghu <anika.raghuvanshi@gmail.com>
2023-10-25 17:20:43 -0700

    devent to alpha, batcher proposer upgrade scripts, styling (#114)

--- setup-templates/template-incident/script/PausePortal.s.sol
+++ setup-templates/template-incident/script/PausePortal.s.sol
@@ -5,12 +5,8 @@ import "@base-contracts/script/universal/MultisigBuilder.sol";
 import "@eth-optimism-bedrock/contracts/L1/OptimismPortal.sol";
 
 contract PausePortal is MultisigBuilder {
-    // address constant internal OPTIMISM_PORTAL_PROXY = <TODO: insert OptimismPortalProxy address>;
-    //      mainnet value: 0x49048044D57e1C92A77f79988d21Fa8fAF74E97e
-    //      goerli value: 0xe93c8cD0D409341205A592f8c4Ac1A5fe5585cfA
-    // address constant internal GUARDIAN = <TODO: insert multisig which is Guardian address>;
-    //      mainnet value: 0x14536667Cd30e52C0b458BaACcB9faDA7046E056
-    //      goerli value: 0x4C35Ca57616E0d5fD808574772f632D8dA4eadCa
+    address constant internal OPTIMISM_PORTAL_PROXY = vm.envAddress("OPTIMISM_PORTAL_PROXY"); // TODO: define OPTIMISM_PORTAL_PROXY=xxx in the .env file
+    address constant internal GUARDIAN = vm.envAddress("GUARDIAN"); // TODO: define GUARDIAN=xxx in the .env file
 
     function _postCheck() internal override view {
         OptimismPortal optimismPortal = OptimismPortal(payable(OPTIMISM_PORTAL_PROXY));
