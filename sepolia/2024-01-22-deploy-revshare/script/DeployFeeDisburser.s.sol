3bfd841224aad3ba91a4d1608de55aba2939ca97
cody-wang <cody.wang@coinbase.com>
2024-01-23 19:13:00 -0500

    deploy revshare to sepolia

---
+++ sepolia/2024-01-22-deploy-revshare/script/DeployFeeDisburser.s.sol
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.15;
+
+import { console } from "forge-std/console.sol";
+import { Script } from "forge-std/Script.sol";
+
+import { Proxy } from "@eth-optimism-bedrock/src/universal/Proxy.sol";
+import { FeeDisburser } from "@base-contracts/src/revenue-share/FeeDisburser.sol";
+
+contract DeployFeeDisburser is Script {
+   function run() external {
+        address deployer = vm.envAddress("FEE_DISBURSER_DEPLOYER");
+        address payable optimismWallet = payable(vm.envAddress("OPTIMISM_WALLET"));
+        address balanceTracker = vm.envAddress("BALANCE_TRACKER_PROXY");
+        uint256 feeDisbursementInterval = vm.envUint("FEE_DISBURSEMENT_INTERVAL");
+        address admin = vm.envAddress("FEE_DISBURSER_ADMIN");
+        string memory salt = vm.envString("FEE_DISBURSER_SALT");
+
+        console.log("Deployer: %s", deployer);
+        console.log("Optimism Wallet: %s", optimismWallet);
+        console.log("Balance Tracker: %s", balanceTracker);
+        console.log("Fee Disbursement Interval: %s", feeDisbursementInterval);
+        console.log("Admin: %s", admin);
+        console.log("Salt: %s", salt);
+
+        vm.broadcast(deployer);
+        FeeDisburser feeDisburserImpl = new FeeDisburser(
+            optimismWallet,
+            balanceTracker,
+            feeDisbursementInterval
+        );
+        require(feeDisburserImpl.OPTIMISM_WALLET() == optimismWallet, "DeployFeeDisburser: incorrect optimism wallet");
+        require(feeDisburserImpl.L1_WALLET() == balanceTracker, "DeployFeeDisburser: incorrect l1 wallet");
+        require(feeDisburserImpl.FEE_DISBURSEMENT_INTERVAL() == feeDisbursementInterval, "DeployFeeDisburser: incorrect fee disbursement interval");
+
+        console.log("Fee Disburser Impl address: %s", address(feeDisburserImpl));
+   }
+}
---
+++ sepolia/2024-01-22-deploy-revshare/script/UpgradeToFeeDisburser.s.sol
@@ -0,0 +1,54 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.15;
+
+import { console } from "forge-std/console.sol";
+
+import { IMulticall3 } from "forge-std/interfaces/IMulticall3.sol";
+
+import { Proxy } from "@eth-optimism-bedrock/src/universal/Proxy.sol";
+
+import { MultisigBuilder } from "@base-contracts/script/universal/MultisigBuilder.sol";
+import { FeeDisburser } from "@base-contracts/src/revenue-share/FeeDisburser.sol";
+
+contract UpgradeToFeeDisburser is MultisigBuilder {
+    address payable internal FEE_DISBURSER_PROXY = payable(vm.envAddress("FEE_DISBURSER_PROXY"));
+    address payable internal FEE_DISBURSER_IMPL = payable(vm.envAddress("FEE_DISBURSER_IMPL"));
+    address internal OPTIMISM_WALLET = vm.envAddress("OPTIMISM_WALLET");
+    address internal CB_SAFE_ADDR = vm.envAddress("CB_SAFE_ADDR");
+
+    function _buildCalls() internal override view returns (IMulticall3.Call3[] memory) {
+        console.log("Fee Diburser Proxy %s", FEE_DISBURSER_PROXY);
+        console.log("Fee Diburser Impl %s", FEE_DISBURSER_IMPL);
+        console.log("Optimism Wallet %s", OPTIMISM_WALLET);
+        console.log("CB_SAFE_ADDR %s", CB_SAFE_ADDR);
+
+        IMulticall3.Call3[] memory calls = new IMulticall3.Call3[](1);
+
+        ///
+        // FEE DISBURSER PROXY UPGRADE TO FEE DISBURSER IMPLEMENTATION CALL
+        ///
+        calls[0] = IMulticall3.Call3({
+            target: FEE_DISBURSER_PROXY,
+            allowFailure: false,
+            callData: abi.encodeCall(
+                Proxy.upgradeTo,
+                (
+                    FEE_DISBURSER_IMPL
+                )
+            )
+        });
+
+        return calls;
+    }
+
+    function _ownerSafe() internal override view returns (address) {
+        return CB_SAFE_ADDR;
+    }
+
+    function _postCheck() internal override view {
+        require(
+            FeeDisburser(FEE_DISBURSER_PROXY).OPTIMISM_WALLET() == OPTIMISM_WALLET,
+            "UpgradeToFeeDisburser: FeeDisburser incorrect Optimism Wallet"
+        );
+    }
+}
