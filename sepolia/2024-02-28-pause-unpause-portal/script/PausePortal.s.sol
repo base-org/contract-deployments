4b52399e83176d8e18001450efb12741ac413bf5
Nadir Akhtar <nadir.akhtar@coinbase.com>
2024-03-04 22:27:44 -0800

    Remove constant type due to compiler issues

--- sepolia/2024-02-28-pause-unpause-portal/script/PausePortal.s.sol
+++ sepolia/2024-02-28-pause-unpause-portal/script/PausePortal.s.sol
@@ -5,8 +5,8 @@ import "forge-std/Script.sol";
 import "@eth-optimism-bedrock/src/L1/OptimismPortal.sol";
 
 contract PausePortal is Script {
-    address constant internal OPTIMISM_PORTAL_PROXY = vm.envAddress("OPTIMISM_PORTAL_PROXY");
-    address constant internal GUARDIAN = vm.envAddress("GUARDIAN");
+    address internal OPTIMISM_PORTAL_PROXY = vm.envAddress("OPTIMISM_PORTAL_PROXY");
+    address internal GUARDIAN = vm.envAddress("GUARDIAN");
 
     function run() external {
         vm.startBroadcast(vm.envUint("GUARDIAN_PRIVATE_KEY"));
