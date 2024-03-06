8f6beb5d3afc667edec5fc68af6ea2d280c052ae
nadir-akhtar-coinbase <64859114+nadir-akhtar-coinbase@users.noreply.github.com>
2024-03-06 10:39:08 -0800

    [sepolia] Incident practice - pause and unpause Sepolia (#126)
    * Add auto-generated contracts and set up .env file
    
    * Remove unneeded files and info
    
    * Remove unneeded Makefile commands
    
    * Refactor for EOA
    
    * Remove constant type due to compiler issues
    
    * Add require statements and slightly refactor
    
    * Update Makefile for EOA signing and broadcasting
    
    * Use addr for broadcasting

---
+++ sepolia/2024-02-28-pause-unpause-portal/script/PausePortal.s.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.15;
+
+import "forge-std/Script.sol";
+import "@eth-optimism-bedrock/src/L1/OptimismPortal.sol";
+
+contract PausePortal is Script {
+    address internal OPTIMISM_PORTAL_PROXY = vm.envAddress("OPTIMISM_PORTAL_PROXY");
+    address internal GUARDIAN = vm.envAddress("GUARDIAN");
+
+    function run() external {
+        OptimismPortal optimismPortal = OptimismPortal(payable(OPTIMISM_PORTAL_PROXY));
+
+        vm.broadcast(GUARDIAN);
+        optimismPortal.pause();
+
+        require(optimismPortal.paused() == true, "PausePortal: failed to pause");
+    }
+}
---
+++ sepolia/2024-02-28-pause-unpause-portal/script/UnpausePortal.s.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.15;
+
+import "forge-std/Script.sol";
+import "@eth-optimism-bedrock/src/L1/OptimismPortal.sol";
+
+contract UnpausePortal is Script {
+    address internal OPTIMISM_PORTAL_PROXY = vm.envAddress("OPTIMISM_PORTAL_PROXY");
+    address internal GUARDIAN = vm.envAddress("GUARDIAN");
+
+    function run() external {
+        OptimismPortal optimismPortal = OptimismPortal(payable(OPTIMISM_PORTAL_PROXY));
+
+        vm.broadcast(GUARDIAN);
+        optimismPortal.unpause();
+
+        require(optimismPortal.paused() == false, "PausePortal: failed to unpause");
+    }
+}
