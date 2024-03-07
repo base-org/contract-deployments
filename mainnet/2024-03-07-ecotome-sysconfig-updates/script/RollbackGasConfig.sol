603d51b9ccd372347ae051d3c89088ef25c9ea04
katzman <steve.katzman@coinbase.com>
2024-03-06 16:50:31 -0800

    Add rollback script

--- mainnet/2024-03-07-ecotome-sysconfig-updates/script/RollbackGasConfig.sol
+++ mainnet/2024-03-07-ecotome-sysconfig-updates/script/RollbackGasConfig.sol
@@ -4,14 +4,14 @@ pragma solidity 0.8.15;
 import {SystemConfig} from "@eth-optimism-bedrock/src/L1/SystemConfig.sol";
 import {MultisigBuilder, IMulticall3} from "@base-contracts/script/universal/MultisigBuilder.sol";
 
-contract UpdateGasConfig is MultisigBuilder {
+contract RollbackGasConfig is MultisigBuilder {
 
     address internal SYSTEM_CONFIG_OWNER = vm.envAddress("SYSTEM_CONFIG_OWNER");
     address internal L1_SYSTEM_CONFIG = vm.envAddress("L1_SYSTEM_CONFIG_ADDRESS");
-    uint256 internal SCALAR = vm.envUint("SCALAR");
+    uint256 internal FALLBACK_SCALAR = vm.envUint("FALLBACK_SCALAR");
 
     function _postCheck() internal override view {
-        require(SystemConfig(L1_SYSTEM_CONFIG).scalar() == SCALAR);
+        require(SystemConfig(L1_SYSTEM_CONFIG).scalar() == FALLBACK_SCALAR);
         require(SystemConfig(L1_SYSTEM_CONFIG).overhead() == 0);
     }
 
@@ -25,7 +25,7 @@ contract UpdateGasConfig is MultisigBuilder {
                 SystemConfig.setGasConfig,
                 (
                 0, // overhead is not used post Ecotome
-                SCALAR
+                FALLBACK_SCALAR
                 )
             )
         });
