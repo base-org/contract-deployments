9301d7a5f42ae354a02ec6837b2b55b876c2f8a2
katzman <steve.katzman@coinbase.com>
2024-03-06 16:39:56 -0800

    Added script and Makefile cmds for updating gas config

---
+++ mainnet/2024-03-07-ecotome-sysconfig-updates/script/UpdateGasConfig.sol
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.15;
+
+import {SystemConfig} from "@eth-optimism-bedrock/src/L1/SystemConfig.sol";
+import "@base-contracts/script/universal/MultisigBuilder.sol";
+
+contract UpdateGasConfig is MultisigBuilder {
+
+    address internal SYSTEM_CONFIG_OWNER = vm.envAddress("SYSTEM_CONFIG_OWNER");
+    address internal L1_SYSTEM_CONFIG = vm.envAddress("L1_SYSTEM_CONFIG_ADDRESS");
+    uint256 internal SCALAR = vm.envUint("SCALAR");
+
+    function _postCheck() internal override view {
+        require(SystemConfig(L1_SYSTEM_CONFIG).scalar() == SCALAR);
+        require(SystemConfig(L1_SYSTEM_CONFIG).overhead() == 0);
+    }
+
+    function _buildCalls() internal override view returns (IMulticall3.Call3[] memory) {
+        IMulticall3.Call3[] memory calls = new IMulticall3.Call3[](1);
+
+        calls[0] = IMulticall3.Call3({
+            target: L1_SYSTEM_CONFIG,
+            allowFailure: false,
+            callData: abi.encodeCall(
+                SystemConfig.setGasConfig,
+                (
+                0, // overhead is not used post Ecotome
+                SCALAR
+                )
+            )
+        });
+
+        return calls;
+    }
+
+    function _ownerSafe() internal override view returns (address) {
+        return SYSTEM_CONFIG_OWNER;
+    }
+}
