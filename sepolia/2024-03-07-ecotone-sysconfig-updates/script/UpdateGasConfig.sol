2ad8b5d23d8d6b14b2739a9b03e61bb11268e424
katzman <steve.katzman@coinbase.com>
2024-03-08 10:58:07 -0800

    Added updateGasConfigSepolia script and associated env/makefile

---
+++ sepolia/2024-03-07-ecotone-sysconfig-updates/script/UpdateGasConfig.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.15;
+
+import {SystemConfig} from "@eth-optimism-bedrock/src/L1/SystemConfig.sol";
+import "forge-std/Script.sol";
+
+contract UpdateGasConfigSepolia is Script {
+
+    address internal L1_SYSTEM_CONFIG = vm.envAddress("L1_SYSTEM_CONFIG_ADDRESS");
+    uint256 internal SCALAR = vm.envUint("SCALAR");
+
+    function _postCheck() internal view {
+        require(SystemConfig(L1_SYSTEM_CONFIG).scalar() == SCALAR);
+        require(SystemConfig(L1_SYSTEM_CONFIG).overhead() == 0);
+    }
+
+    function run() public {
+        vm.startBroadcast();
+        SystemConfig(L1_SYSTEM_CONFIG).setGasConfig(0, SCALAR); 
+        _postCheck();
+        vm.stopBroadcast(); 
+    }
+}
