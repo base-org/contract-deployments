82171e79f7d4b578891f4cb5c81c473c24c57a89
Wilson Cusack <wilson.cusack@coinbase.com>
2024-04-12 14:35:51 -0400

    verification working

--- sepolia/2024-04-10-deploy-ERC20Factory/script/RunERC20FactoryDeploy.s.sol
+++ sepolia/2024-04-10-deploy-ERC20Factory/script/RunERC20FactoryDeploy.s.sol
@@ -3,7 +3,7 @@
 pragma solidity 0.8.15;
 
 import "forge-std/Script.sol";
-import "@eth-optimism-bedrock/src/universal/OptimismMintableERC20Factory.sol";
+import {OptimismMintableERC20Factory} from "@eth-optimism-bedrock/src/universal/OptimismMintableERC20Factory.sol";
 
 contract RunERC20FactoryDeploy is Script {
     function run() public {
