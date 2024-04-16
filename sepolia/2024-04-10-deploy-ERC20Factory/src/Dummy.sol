d737cf497952e79bc7105118a3d3d6eb9bf3b4e6
Wilson Cusack <wilson.cusack@coinbase.com>
2024-04-16 11:26:00 -0400

    mainnet deploy

--- sepolia/2024-04-10-deploy-ERC20Factory/src/Dummy.sol
+++
@@ -1,6 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity 0.8.15;
-
-// This file with this import is needed for `make verify` to work.
-// seems like Foundry issue
-import {OptimismMintableERC20Factory} from "@eth-optimism-bedrock/src/universal/OptimismMintableERC20Factory.sol";
