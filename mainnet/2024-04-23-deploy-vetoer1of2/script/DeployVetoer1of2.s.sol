20682d2f0e201f17e1510a74a0a77d758804dfb5
Baptiste Oueriagli <baptiste.oueriagli@coinbase.com>
2024-04-23 12:14:18 +0000

    deploy Vetoer1of2

---
+++ mainnet/2024-04-23-deploy-vetoer1of2/script/DeployVetoer1of2.s.sol
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.15;
+
+import "forge-std/Script.sol";
+
+import {Vetoer1of2} from "@base-contracts/src/Vetoer1of2.sol";
+
+contract DeployVetoer1of2 is Script {
+    function deployL1() public {
+        address opSafe = vm.envAddress("L1_OP_SAFE");
+        address baseSafe = vm.envAddress("L1_BASE_SAFE");
+        address initiator = vm.envAddress("L1_SECURITY_COUNCIL_SAFE");
+        address proxyAdmin = vm.envAddress("L1_PROXY_ADMIN");
+
+        _deployVetoer1of2({opSafe: opSafe, baseSafe: baseSafe, initiator: initiator, proxyAdmin: proxyAdmin});
+    }
+
+    function deployL2() public {
+        address opSafe = vm.envAddress("L2_OP_SAFE");
+        address baseSafe = vm.envAddress("L2_BASE_SAFE");
+        address initiator = vm.envAddress("L2_SECURITY_COUNCIL_SAFE");
+        address proxyAdmin = vm.envAddress("L2_PROXY_ADMIN");
+
+        _deployVetoer1of2({opSafe: opSafe, baseSafe: baseSafe, initiator: initiator, proxyAdmin: proxyAdmin});
+    }
+
+    function _deployVetoer1of2(address opSafe, address baseSafe, address initiator, address proxyAdmin) private {
+        vm.startBroadcast();
+
+        Vetoer1of2 vetoer1of2 =
+            new Vetoer1of2({opSigner_: opSafe, otherSigner_: baseSafe, initiator: initiator, target: proxyAdmin});
+
+        vm.stopBroadcast();
+
+        console.log("Vetoer1of2 deployed at", address(vetoer1of2));
+        console.log("DelayedVetoable deployed at", vetoer1of2.delayedVetoable());
+    }
+}
