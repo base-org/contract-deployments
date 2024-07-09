1bd9148e3d255d6afb04fdd61cfe9a76406d8823
Nadir Akhtar <nadir.akhtar@coinbase.com>
2024-07-09 16:49:35 -0700

    Update files and remove artifacts

--- sepolia/2024-07-09-migrate-bridge-ownership/script/VerifyContractState.s.sol
+++ sepolia/2024-07-09-migrate-bridge-ownership/script/VerifyContractState.s.sol
@@ -2,8 +2,10 @@
 pragma solidity 0.8.15;
 
 import "forge-std/Script.sol";
+import "forge-std/console.sol";
 
 import "@eth-optimism-bedrock/src/dispute/AnchorStateRegistry.sol";
+import {Constants} from "@eth-optimism-bedrock/src/libraries/Constants.sol";
 
 contract VerifyContractState is Script {
     function setUp() public {}
@@ -16,19 +18,45 @@ contract VerifyContractState is Script {
         verifyPermissionedDisputeGame();
     }
 
-    function verifyAnchorStateRegistry() internal returns (bool) {
+    function verifyAnchorStateRegistry() internal view {
+        address addr = vm.envAddress("ANCHOR_STATE_REGISTRY_PROXY");
+
+        address impl = bytes32ToAddress(vm.load(addr, Constants.PROXY_IMPLEMENTATION_ADDRESS));
+
+        AnchorStateRegistry anchorStateRegistry = AnchorStateRegistry(addr);
+
+        string memory version = anchorStateRegistry.version();
+        address disputeGameFactoryAddr = address(anchorStateRegistry.disputeGameFactory());
+
+        // Note that Hash is a custom type around bytes32
+        (Hash rootCannon, uint256 l2BlockNumberCannon) = anchorStateRegistry.anchors(GameType.wrap(0));
+        (Hash rootPermissionedCannon, uint256 l2BlockNumberPermissionedCannon) = anchorStateRegistry.anchors(GameType.wrap(1));
+
+        console.log("AnchorStateRegistry: %s", addr);
+        console.log("- Implementation address: %s", impl);
+        console.log("- Version: %s", version);
+        console.log("- DisputeGameFactory address: %s", disputeGameFactoryAddr);
+        console.log("- Anchor for GameType.CANNON: %s, %s", vm.toString(Hash.unwrap(rootCannon)), l2BlockNumberCannon);
+        console.log("- Anchor for GameType.PERMISSIONED_CANNON: %s, %s", vm.toString(Hash.unwrap(rootPermissionedCannon)), l2BlockNumberPermissionedCannon);
+    }
+
+    function verifyDelayedWETH() internal {
         // TODO: Implement
     }
-    function verifyDelayedWETH() internal returns (bool) {
+
+    function verifyDisputeGameFactory() internal {
         // TODO: Implement
     }
-    function verifyDisputeGameFactory() internal returns (bool) {
+
+    function verifyFaultDisputeGame() internal {
         // TODO: Implement
     }
-    function verifyFaultDisputeGame() internal returns (bool) {
+
+    function verifyPermissionedDisputeGame() internal {
         // TODO: Implement
     }
-    function verifyPermissionedDisputeGame() internal returns (bool) {
-        // TODO: Implement
+
+    function bytes32ToAddress(bytes32 _bytes32) internal pure returns (address) {
+        return address(uint160(uint256(_bytes32)));
     }
 }
