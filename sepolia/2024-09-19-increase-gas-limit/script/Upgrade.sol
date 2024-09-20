ce99b9e583babe11db0f1d453935a6704e6b635e
Brian Bland <brian.t.bland@gmail.com>
2024-09-20 12:50:05 -0700

    Example: Simple gas limit increase with rollback on Sepolia

---
+++ sepolia/2024-09-19-increase-gas-limit/script/Upgrade.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.15;
+
+import { SetGasLimitBuilder } from "@base-contracts/script/deploy/l1/SetGasLimitBuilder.sol";
+
+contract Upgrade is SetGasLimitBuilder {
+    uint64 internal OLD_GAS_LIMIT = uint64(vm.envUint("OLD_GAS_LIMIT"));
+    uint64 internal NEW_GAS_LIMIT = uint64(vm.envUint("NEW_GAS_LIMIT"));
+
+    function _fromGasLimit() internal view override returns (uint64) {
+        return OLD_GAS_LIMIT;
+    }
+
+    function _toGasLimit() internal view override returns (uint64) {
+        return NEW_GAS_LIMIT;
+    }
+
+    function _nonceOffset() internal pure override returns (uint64) {
+        return 0;
+    }
+}
