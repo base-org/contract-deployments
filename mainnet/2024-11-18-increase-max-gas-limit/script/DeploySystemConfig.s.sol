a7f5da89da54212fd052915707f31c24d127ae3e
Baptiste Oueriagli <baptiste.oueriagli@coinbase.com>
2024-11-22 17:45:57 +0000

    fix compile errors

--- mainnet/2024-11-18-increase-max-gas-limit/script/DeploySystemConfig.s.sol
+++ mainnet/2024-11-18-increase-max-gas-limit/script/DeploySystemConfig.s.sol
@@ -1,14 +1,15 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.15;
 
-import "@eth-optimism-bedrock/contracts/L1/SystemConfig.sol";
-import "forge-std/Script.sol";
+import {Script, console} from "forge-std/Script.sol";
+
+import {SystemConfig} from "@eth-optimism-bedrock/src/L1/SystemConfig.sol";
 
 contract DeploySystemConfig is Script {
     function run() public {
         vm.startBroadcast();
         SystemConfig systemConfigImpl = new SystemConfig();
-	console.log("SystemConfig implementation deployed at: ", address(systemConfigImpl));
-	vm.stopBroadcast();
+        console.log("SystemConfig implementation deployed at: ", address(systemConfigImpl));
+        vm.stopBroadcast();
     }
 }
--- mainnet/2024-11-18-increase-max-gas-limit/script/UpgradeSystemConfig.s.sol
+++ mainnet/2024-11-18-increase-max-gas-limit/script/UpgradeSystemConfig.s.sol
@@ -1,36 +1,39 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.15;
 
-import { IMulticall3 } from "forge-std/interfaces/IMulticall3.sol";
-import { SystemConfig } from "@eth-optimism-bedrock/contracts/L1/SystemConfig.sol";
-import { MultisigBuilder } from "@base-contracts/script/universal/MultisigBuilder.sol";
+import {Vm} from "forge-std/Vm.sol";
+import {IMulticall3} from "forge-std/interfaces/IMulticall3.sol";
+
+import {SystemConfig} from "@eth-optimism-bedrock/src/L1/SystemConfig.sol";
+import {MultisigBuilder, Simulation} from "@base-contracts/script/universal/MultisigBuilder.sol";
+
+interface IProxyAdmin {
+    function implementation() external view returns (address);
+    function upgradeTo(address _implementation) external;
+}
 
 contract TransferSystemConfigOwner is MultisigBuilder {
-    address internal SYSTEM_CONFIG_OWNER = vm.envAddress("SYSTEM_CONFIG_OWNER")
+    address internal SYSTEM_CONFIG_OWNER = vm.envAddress("SYSTEM_CONFIG_OWNER");
     address internal SYSTEM_CONFIG_ADDRESS = vm.envAddress("SYSTEM_CONFIG_ADDRESS");
-    address constant internal NEW_IMPLEMENTATION = vm.envAddress("NEW_IMPLEMENTATION");
+    address internal NEW_IMPLEMENTATION = vm.envAddress("NEW_IMPLEMENTATION");
 
-    function _postCheck() internal override view {
-        require(SystemConfig(SYSTEM_CONFIG_ADDR).implementation() == NEW_IMPLEMENTATION);
+    function _postCheck(Vm.AccountAccess[] memory, Simulation.Payload memory) internal view override {
+        require(IProxyAdmin(SYSTEM_CONFIG_ADDRESS).implementation() == NEW_IMPLEMENTATION);
     }
 
-    function _buildCalls() internal override pure returns (IMulticall3.Call3[] memory) {
+    function _buildCalls() internal view override returns (IMulticall3.Call3[] memory) {
         IMulticall3.Call3[] memory calls = new IMulticall3.Call3[](1);
 
         calls[0] = IMulticall3.Call3({
-            target: SYTEM_CONFIG_ADDRESS,
+            target: SYSTEM_CONFIG_ADDRESS,
             allowFailure: false,
-            callData: abi.encodeCall(
-                SystemConfig.upgradeTo,
-                (NEW_IMPLEMENTATION)
-            )
+            callData: abi.encodeCall(IProxyAdmin.upgradeTo, (NEW_IMPLEMENTATION))
         });
 
         return calls;
     }
 
-    function _ownerSafe() internal override view returns (address) {
+    function _ownerSafe() internal view override returns (address) {
         return SYSTEM_CONFIG_OWNER;
     }
 }
-
