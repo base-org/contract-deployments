51ff16b65124d75c6936603b29f523381043ab3a
Baptiste Oueriagli <baptiste.oueriagli@coinbase.com>
2024-11-22 20:15:55 +0000

    fix addresses + working script

--- mainnet/2024-11-18-increase-max-gas-limit/script/UpgradeSystemConfig.s.sol
+++ mainnet/2024-11-18-increase-max-gas-limit/script/UpgradeSystemConfig.s.sol
@@ -8,33 +8,39 @@ import {SystemConfig} from "@eth-optimism-bedrock/src/L1/SystemConfig.sol";
 import {MultisigBuilder, Simulation} from "@base-contracts/script/universal/MultisigBuilder.sol";
 
 interface IProxyAdmin {
+    function upgrade(address _proxy, address _implementation) external;
+}
+
+interface IProxy {
     function implementation() external view returns (address);
-    function upgradeTo(address _implementation) external;
 }
 
 contract UpgradeSystemConfig is MultisigBuilder {
-    address internal SYSTEM_CONFIG_OWNER = vm.envAddress("SYSTEM_CONFIG_OWNER");
+    address internal SAFE_ADDRESS = vm.envAddress("SAFE_ADDRESS");
+    address internal PROXY_ADMIN_ADDRESS = vm.envAddress("PROXY_ADMIN_ADDRESS");
     address internal SYSTEM_CONFIG_ADDRESS = vm.envAddress("SYSTEM_CONFIG_ADDRESS");
     address internal NEW_IMPLEMENTATION = vm.envAddress("NEW_IMPLEMENTATION");
 
-    function _postCheck(Vm.AccountAccess[] memory, Simulation.Payload memory) internal view override {
-        require(IProxyAdmin(SYSTEM_CONFIG_ADDRESS).implementation() == NEW_IMPLEMENTATION);
+    function _postCheck(Vm.AccountAccess[] memory, Simulation.Payload memory) internal override {
+        // NOTE: Bypass `proxyCallIfNotAdmin` modifier.
+        vm.prank(PROXY_ADMIN_ADDRESS);
+        require(IProxy(SYSTEM_CONFIG_ADDRESS).implementation() == NEW_IMPLEMENTATION);
     }
 
     function _buildCalls() internal view override returns (IMulticall3.Call3[] memory) {
         IMulticall3.Call3[] memory calls = new IMulticall3.Call3[](3);
 
-        // FIXME: Doe snot work as the SystemConfig is gated by `proxyCallIfNotAdmin`.
         calls[0] = IMulticall3.Call3({
-            target: SYSTEM_CONFIG_ADDRESS,
+            target: PROXY_ADMIN_ADDRESS,
             allowFailure: false,
-            callData: abi.encodeCall(IProxyAdmin.upgradeTo, (NEW_IMPLEMENTATION))
+            // NOTE: No need to call initialize as no storage would change (only changing `MAX_GAS_LIMIT` and `version`).
+            callData: abi.encodeCall(IProxyAdmin.upgrade, (SYSTEM_CONFIG_ADDRESS, NEW_IMPLEMENTATION))
         });
 
         return calls;
     }
 
     function _ownerSafe() internal view override returns (address) {
-        return SYSTEM_CONFIG_OWNER;
+        return SAFE_ADDRESS;
     }
 }
